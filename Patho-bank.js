96e0ffac23e4d0f4814ea4adef2a4abbe21be73d
poulaoe <46631525+poulaoe@users.noreply.github.com>
2026-07-01 13:14:05 +0000

    fpen

--- Patho-bank.js
+++ Patho-bank.js
@@ -512,10 +512,5 @@ window.PATHO_BANK = window.PATHO_BANK.map(function(item) {
   };
 });
 window.BANK = window.PATHO_BANK;
-const QC_BANK = window.QC_BANK.map(function(item) {
-    return {
-        q: item.q,
-        r: item.r
-    };
-     window.QC_BANK = window.QC_BANK
+
 });
