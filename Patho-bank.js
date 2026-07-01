649160fb2e6233df28c16bf24d28ae758915bb23
poulaoe <46631525+poulaoe@users.noreply.github.com>
2026-07-01 13:00:39 +0000

    lellel

--- Patho-bank.js
+++ Patho-bank.js
@@ -512,7 +512,7 @@ window.PATHO_BANK = window.PATHO_BANK.map(function(item) {
   };
 });
 window.BANK = window.PATHO_BANK;
-window.QC_BANK = window._QC_BANK.map(function(item) {
+const QC_BANK = window._QC_BANK.map(function(item) {
     return {
         q: item.q,
         r: item.r
